1fd8c0ee95a03fbaf639089d91b0dea7436bcdf5
Annya <annya.host@gmail.com>
2025-01-15 10:08:22 +0800

    add basic cfg test

--- src/cfg.rs
+++ src/cfg.rs
@@ -6,13 +6,13 @@ pub struct BrilCFG {
     bril: Bril,
     unamed_block_cnt: usize,
     cur_name: Option<String>,
-    pub blocks: Vec<Block>
+    pub blocks: Vec<Block>,
 }
 
 pub struct Block {
     name: String,
     instrs: Vec<Instr>,
-    succ: Option<Vec<String>>
+    succ: Option<Vec<String>>,
 }
 
 const TERMINATOR: [Opcode; 3] = [Opcode::jmp, Opcode::ret, Opcode::br];
@@ -23,6 +23,7 @@ impl Display for Block {
         for instr in &self.instrs {
             writeln!(f, "\t{:?}:", instr).unwrap();
         }
+        writeln!(f, "succ: {:?}", self.succ).unwrap();
         Ok(())
     }
 }
@@ -33,7 +34,7 @@ impl BrilCFG {
             bril,
             unamed_block_cnt: 0,
             cur_name: None,
-            blocks: vec![]
+            blocks: vec![],
         }
     }
     pub fn resolve_cfg(&mut self) {
@@ -41,9 +42,9 @@ impl BrilCFG {
             if let Some(last) = block.instrs.last() {
                 use crate::parser::Instr::*;
                 let mut succ = None;
-                if let Instruction {op, labels, ..} = last {
+                if let Instruction { op, labels, .. } = last {
                     if [Opcode::jmp, Opcode::br].contains(op) {
-                        let labels  = labels.as_ref().unwrap();
+                        let labels = labels.as_ref().unwrap();
                         succ = Some(labels.clone())
                     } else if op == &Opcode::ret {
                         succ = None
@@ -57,7 +58,7 @@ impl BrilCFG {
                     }
                     let ptr = self.blocks.as_ptr() as *mut Block;
                     unsafe {
-                        // cur block will never be used afterwards, so it's safe to 
+                        // cur block will never be used afterwards, so it's safe to
                         // change current block
                         // so unsafe code is applied to bypass the dumb checking system
                         (*ptr.add(cnt)).succ = succ.clone();
@@ -70,7 +71,7 @@ impl BrilCFG {
             }
         }
     }
-    pub fn parse_blocks(&mut self)  {
+    pub fn parse_blocks(&mut self) {
         let mut instrs = vec![];
         for func in self.bril.functions.clone() {
             self.set_cur_block_name(&func.name);
@@ -80,24 +81,37 @@ impl BrilCFG {
                     Instruction { op, .. } => {
                         instrs.push(instr.clone());
                         if TERMINATOR.contains(op) {
-                            let block = Block {name: self.cur_block_name(), instrs: instrs.clone(), succ: None};
+                            let block = Block {
+                                name: self.cur_block_name(),
+                                instrs: instrs.clone(),
+                                succ: None,
+                            };
                             self.blocks.push(block);
                             instrs.clear();
                         }
-                    },
+                    }
                     Label { label } => {
-                        let block = Block {name: self.cur_block_name(), instrs: instrs.clone(), succ: None};
+                        let block = Block {
+                            name: self.cur_block_name(),
+                            instrs: instrs.clone(),
+                            succ: None,
+                        };
                         self.blocks.push(block);
                         instrs.clear();
                         self.set_cur_block_name(label);
-                    },
+                    }
                 }
             }
         }
         if !instrs.is_empty() {
-            let block = Block {name: self.cur_block_name(), instrs: instrs.clone(), succ: None};
+            let block = Block {
+                name: self.cur_block_name(),
+                instrs: instrs.clone(),
+                succ: None,
+            };
             self.blocks.push(block);
         }
+        self.resolve_cfg();
     }
 
     fn set_cur_block_name(&mut self, name: &str) {
@@ -107,6 +121,7 @@ impl BrilCFG {
         if let Some(name) = self.cur_name.take() {
             name
         } else {
+            // FIXME: what is some label is start with tmp?
             let tmp_name = format!("tmp{}", self.unamed_block_cnt);
             self.unamed_block_cnt += 1;
             tmp_name
@@ -116,11 +131,46 @@ impl BrilCFG {
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        io::Write,
+        process::{Command, Stdio},
+    };
+
     use super::*;
+    // TODO: test on bril's test directory
 
     #[test]
     fn cfg_jmp() {
+        let bril_text = r#"@main{
+        v: int = const 4;
+        jmp .somewhere;
+        v: int = const 2;
+        .somewhere:
+        print v;
+}"#;
 
+        let mut command = Command::new("bril2json")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to execute command");
+        if let Some(mut stdin) = command.stdin.take() {
+            stdin
+                .write_all(bril_text.as_bytes())
+                .expect("Failed to write to stdin");
+        }
+        let bril_json = command
+            .wait_with_output()
+            .expect("Failed to wait on bril2json")
+            .stdout;
+        let bril_json = String::from_utf8(bril_json).expect("invalid string");
+        println!("bril_json: {bril_json}");
+
+        let bril: Bril = serde_json::from_str(&bril_json).unwrap();
+        let mut cfg = BrilCFG::new(bril);
+        cfg.parse_blocks();
+        for block in cfg.blocks {
+            println!("{block}");
+        }
     }
 }
-
